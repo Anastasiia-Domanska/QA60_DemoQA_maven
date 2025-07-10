de2954772b58e864239b4d89f8485e6d2ff38a52
Anastasiia-Domanska <akvadelf24@gmail.com>
2025-07-10 19:47:16 +0200

    add Cl.W.25: dragdropp, menu, slider pages tests

--- src/main/java/com/demoqa/pages/HomePage.java
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -9,25 +9,31 @@ public class HomePage extends BasePage{
     public HomePage(WebDriver driver) {
         super(driver);
     }
-
     @FindBy(css = ".card:nth-child(6)")
     WebElement bookstore;
+
     public SidePanel getBookStore() {
         click(bookstore);
+        //  clickWithJS(bookstore, 0, 300);
         return new SidePanel(driver);
     }
-
     @FindBy(css = ".card:nth-child(3)")
     WebElement alertsFrameWindows;
     public SidePanel getAlertsFrameWindows() {
-        clickWithJS(alertsFrameWindows, 0, 300);
+        clickWithJS(alertsFrameWindows, 0, 600);
         return new SidePanel(driver);
-    }
 
+    }
     @FindBy(css = ".card:nth-child(4)")
     WebElement widgets;
     public SidePanel getWidgets() {
         clickWithJS(widgets, 0, 300);
+        return new SidePanel(driver);
+    }
+    @FindBy(css = ".card:nth-child(5)")
+    WebElement interactions;
+    public SidePanel getInteractions() {
+        clickWithJS(interactions,0,300);
         return new SidePanel(driver);
     }
 }
--- src/main/java/com/demoqa/pages/SidePanel.java
+++ src/main/java/com/demoqa/pages/SidePanel.java
@@ -4,56 +4,75 @@ import com.demoqa.pages.alertsFrameWindows.AlertsPage;
 import com.demoqa.pages.alertsFrameWindows.BrowserWindows;
 import com.demoqa.pages.alertsFrameWindows.FramePage;
 import com.demoqa.pages.bookstore.LoginPage;
+import com.demoqa.pages.interactions.DroppablePage;
+import com.demoqa.pages.widgets.MenuPage;
 import com.demoqa.pages.widgets.SelectMenuPage;
+import com.demoqa.pages.widgets.SliderPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class SidePanel extends BasePage{
-
     public SidePanel(WebDriver driver) {
         super(driver);
     }
-
-    @FindBy(xpath = "//span[.='Login']")
+    @FindBy( xpath = "//span[.='Login']")
     WebElement login;
     public LoginPage selectLogin() {
         click(login);
+        // clickWithJS(login, 0, 600);
         return new LoginPage(driver);
     }
 
-    @FindBy(xpath = "//span[.='Alerts']")
+    @FindBy( xpath = "//span[.='Alerts']")
     WebElement alerts;
     public AlertsPage selectAlerts() {
-        clickWithJS(alerts, 0, 300);
+        click(alerts);
         return new AlertsPage(driver);
-    }
 
-    @FindBy(xpath = "//span[.='Frames']")
+    }
+    @FindBy( xpath = "//span[.='Frames']")
     WebElement frames;
     public SidePanel selectFrame() {
-        clickWithJS(frames, 0, 300);
+        clickWithJS(frames,0, 300);
         return new SidePanel(driver);
     }
 
-    @FindBy(xpath = "//span[.='Nested Frames']")
+    @FindBy( xpath = "//span[.='Nested Frames']")
     WebElement nestedFrames;
     public FramePage selectNestedFrames() {
-        clickWithJS(nestedFrames, 0, 300);
+        clickWithJS(nestedFrames,0,300);
         return new FramePage(driver);
     }
-
-    @FindBy(xpath = "//span[.='Browser Windows']")
+    @FindBy( xpath = "//span[.='Browser Windows']")
     WebElement browserWindows;
     public BrowserWindows selectBrowserWindows() {
         clickWithJS(browserWindows, 0, 300);
         return new BrowserWindows(driver);
     }
-
-    @FindBy(xpath = "//span[.='Select Menu']")
+    @FindBy( xpath = "//span[.='Select Menu']")
     WebElement selectMenu;
     public SelectMenuPage getSelectMenu() {
-        clickWithJS(selectMenu, 0, 600);
+        clickWithJS(selectMenu,0,600);
         return new SelectMenuPage(driver);
+    }
+    @FindBy( xpath = "//span[.='Menu']")
+    WebElement menu;
+    public MenuPage getMenu() {
+        clickWithJS(menu,0,600);
+
+        return new MenuPage(driver);
+    }
+    @FindBy( xpath = "//span[.='Slider']")
+    WebElement slider;
+    public SliderPage selectSlider() {
+        clickWithJS(slider,0,400);
+        return new SliderPage(driver);
+    }
+    @FindBy( xpath = "//span[.='Droppable']")
+    WebElement droppable;
+    public DroppablePage selectDroppable() {
+        clickWithJS(droppable,0,600);
+        return new DroppablePage(driver);
     }
 }
